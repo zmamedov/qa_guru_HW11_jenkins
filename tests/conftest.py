e366a5df8e778af4114662e49810c60bc666a4d0
Zaur Mamedov <zaurmd1996@gmail.com>
2024-03-14 19:51:18 +0300

    Update conftest and add allure report

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,37 @@
 import pytest
-from selene import browser
+from selenium import webdriver
+from selene import Browser, Config, browser
+from selenium.webdriver.chrome.options import Options
+
+from utils import attach
 
 
-@pytest.fixture(autouse=True)
-def browser_management():
+@pytest.fixture(scope='function', autouse=True)
+def settings_browser():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "122.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
     browser.config.base_url = 'https://demoqa.com/'
     browser.config.window_height = 1080
     browser.config.window_width = 1920
 
     yield
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
 
     browser.quit()
